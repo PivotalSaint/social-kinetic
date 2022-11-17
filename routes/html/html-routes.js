8874de3b6fcb939d7fd46999963ac2a94f063f12
PivotalSaint <licensedmt@att.net>
2022-11-16 21:28:45 -0600

    done with project

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -5,9 +5,9 @@ router.get("/users", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/social-kinetic.html"));
 });
 
-router.get("/thoughts", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/thought-list.html"));
-});
+// router.get("/thoughts", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../../public/thought-list.html"));
+// });
 
 router.get("/social", (req, res) => {
   res.sendFile(path.join(__dirname, "../../../public/social-kinetic.html"));
